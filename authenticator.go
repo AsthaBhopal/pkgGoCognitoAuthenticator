9f1495bdc765385c6c07574ac0a13377d9bf79e7
shauryam <shauryam@asthatrade.com>
2023-02-13 16:27:40 +0530

    first

---
+++ authenticator.go
@@ -0,0 +1,39 @@
+package authenticator
+
+import (
+	"context"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/cognitoidentityprovider"
+)
+
+type CognitoAuth struct {
+	poolId string
+	client *cognitoidentityprovider.Client
+}
+
+type InitializeParams struct {
+	PoolId string
+	Config aws.Config
+}
+
+func (c *CognitoAuth) Initialize(params InitializeParams) {
+	c.poolId = params.PoolId
+	c.client = cognitoidentityprovider.NewFromConfig(params.Config)
+}
+
+func (c *CognitoAuth) AuthenticateUser(ctx context.Context, token string) (*cognitoidentityprovider.GetUserOutput, error) {
+	return c.client.GetUser(ctx, &cognitoidentityprovider.GetUserInput{
+		AccessToken: &token,
+	})
+}
+
+func (c *CognitoAuth) GetUserGroup(ctx context.Context, userName string, limit int, nextToken string) (*cognitoidentityprovider.AdminListGroupsForUserOutput, error) {
+	l := int32(limit)
+	return c.client.AdminListGroupsForUser(ctx, &cognitoidentityprovider.AdminListGroupsForUserInput{
+		UserPoolId: &c.poolId,
+		Username:   &userName,
+		Limit:      &l,
+		NextToken:  &nextToken,
+	})
+}
---
+++ go.mod
@@ -0,0 +1,12 @@
+module github.com/AsthaBhopal/pkgGoCognitoAuthenticator
+
+go 1.18
+
+require github.com/aws/aws-sdk-go-v2/service/cognitoidentityprovider v1.22.1
+
+require (
+	github.com/aws/aws-sdk-go-v2 v1.17.4 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.1.28 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.4.22 // indirect
+	github.com/aws/smithy-go v1.13.5 // indirect
+)
